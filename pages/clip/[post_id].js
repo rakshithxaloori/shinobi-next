97c64e26ae2361113cf501a26aadb2c3c354795d
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2022-02-23 18:56:11 +0530

    post details styling init

--- pages/clip/[post_id].js
+++ pages/clip/[post_id].js
@@ -1,14 +1,21 @@
 import Head from "next/head";
+import Image from "next/image";
 
 import VideoJS from "components/video";
 import ClipNotFound from "components/clipnotfound";
 
+import styles from "styles/Clip.module.css";
+
 import { createAPIKit } from "utils/APIKit";
 import { clip_cdn_url, create_clip_url, create_embed_url } from "utils/urls";
+import { dateTimeDiff } from "utils/date";
+
+let PROFILE_ICON_SIZE = 50;
+let GAME_ICON_SIZE = 20;
 
 const Clip = ({ post, videoOptions }) => {
   return typeof post?.id === "string" ? (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>{post.title} | Shinobi</title>
         <meta
@@ -28,6 +35,43 @@ const Clip = ({ post, videoOptions }) => {
         <meta property="og:video:height" content={post.clip.height} />
         <meta property="og:video:width" content={post.clip.width} />
       </Head>
+      <div className={styles.header}>
+        <Image
+          className={styles.picture}
+          height={PROFILE_ICON_SIZE}
+          width={PROFILE_ICON_SIZE}
+          alt={post.posted_by.username}
+          src={post.posted_by.picture}
+        />
+        <div className={styles.names}>
+          <div className={styles.username}>
+            <span className={styles.titleText}>{post.posted_by.username}</span>
+            <span className={styles.titleText}>{" \u0387 "}</span>
+            <span className={styles.titleText}>
+              {dateTimeDiff(post.created_datetime)} ago
+            </span>
+          </div>
+          <div className={styles.game}>
+            <Image
+              className={styles.gameIcon}
+              height={GAME_ICON_SIZE}
+              width={GAME_ICON_SIZE}
+              alt={post.game.name}
+              src={post.game.logo_url}
+            />
+            <span className={styles.subtitleText}>{post.game.name}</span>
+          </div>
+        </div>
+      </div>
+      <div className={styles.details}>
+        <span className={styles.titleText}>{post.title}</span>
+        {post.tags.length > 0 && (
+          <span className={styles.subtitleText}>
+            - with {post.tags[0].username}{" "}
+            {post.tags.length > 1 && `${post.tags.length - 1} others`}
+          </span>
+        )}
+      </div>
       <VideoJS options={videoOptions} />
     </div>
   ) : (
