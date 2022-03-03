423b6c0a27641904d0bbe316df95aa94015f2cd7
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2022-03-03 10:53:12 +0530

    config env

--- next.config.js
+++ next.config.js
@@ -21,6 +21,17 @@ const nextConfig = {
       },
     ];
   },
+  env: {
+    CI_CD_STAGE: process.env.CI_CD_STAGE,
+    BASE_API_ENDPOINT: process.env.BASE_API_ENDPOINT,
+    API_KEY: process.env.process,
+
+    GOOGLE_ID: process.env.GOOGLE_ID,
+    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
 };
 
 module.exports = nextConfig;
