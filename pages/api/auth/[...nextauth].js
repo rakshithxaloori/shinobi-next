eb15d5abd28a4c0c2580a846de39ee1353e8df89
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2022-03-03 11:23:13 +0530

    google auth consent

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,6 +11,13 @@ export default async function auth(req, res) {
       GoogleProvider({
         clientId: process.env.GOOGLE_ID,
         clientSecret: process.env.GOOGLE_SECRET,
+        authorization: {
+          params: {
+            prompt: "consent",
+            access_type: "offline",
+            response_type: "code",
+          },
+        },
       }),
     ],
 
