24807ad3474318195f239afe54a300ea85bed69e
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2022-03-04 13:20:27 +0530

    add url

--- utils/urls.js
+++ utils/urls.js
@@ -1,8 +1,10 @@
+const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL;
+
 export const create_clip_url = (post_id) =>
-  `https://shinobi.cc/clip/${post_id}`;
+  `${NEXT_PUBLIC_URL}/clip/${post_id}`;
 
 export const create_embed_url = (post_id) =>
-  `https://shinobi.cc/embed/${post_id}`;
+  `${NEXT_PUBLIC_URL}/embed/${post_id}`;
 
 export const clip_cdn_url = (templateUrl) => {
   let videoUri = templateUrl;
